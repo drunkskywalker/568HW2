2b1c2c879e4cf8c6e12816e6ce2291ec414f060a
Zhengge Tang <zhengge.tang@duke.edu>
2023-02-22 12:07:28 -0500

    v0.2

--- test.cpp
+++ test.cpp
@@ -37,38 +37,46 @@ class Proxy {
   }
 
   void transmit(tcp::socket * user_sock) {
-    asio::streambuf u_buffer;
+    try {
+      asio::streambuf u_buffer;
 
-    asio::read_until(*user_sock, u_buffer, "\r\n\r\n");
-    string request(asio::buffers_begin(u_buffer.data()),
-                   asio::buffers_end(u_buffer.data()));
+      asio::read_until(*user_sock, u_buffer, "\r\n\r\n");
+      string request(asio::buffers_begin(u_buffer.data()),
+                     asio::buffers_end(u_buffer.data()));
 
-    http::request<http::string_body> req;
-    http::request_parser<http::string_body> parser;
-    parser.put(asio::buffer(request.data(), request.size()), err);
-    req = parser.get();
-    cout << req.at("HOST") << endl;
-    string hostname = string(req.at("HOST"));
-    string port = "80";
-    asio::streambuf s_buffer;
-    beast::tcp_stream stream(io_context);
+      http::request<http::string_body> req;
+      http::request_parser<http::string_body> parser;
+      parser.put(asio::buffer(request.data(), request.size()), err);
+      req = parser.get();
+      cout << req.at("HOST") << endl;
+      string hostname = string(req.at("HOST"));
+      string port = "80";
+      asio::streambuf s_buffer;
+      beast::tcp_stream stream(io_context);
 
-    tcp::resolver resolver(io_context);
-    tcp::socket server_sock(io_context);
-    asio::connect(server_sock, resolver.resolve(hostname, port));
+      tcp::resolver resolver(io_context);
+      tcp::socket server_sock(io_context);
+      asio::connect(server_sock, resolver.resolve(hostname, port));
 
-    stream.connect(resolver.resolve(hostname, port));
+      stream.connect(resolver.resolve(hostname, port));
 
-    http::write(stream, req);
-    http::response<http::dynamic_body> res;
+      http::write(stream, req);
+      http::response<http::dynamic_body> res;
 
-    string response_string;
-    beast::flat_buffer buffer;
-    http::read(stream, buffer, res);
-    stringstream ss;
-    ss << res;
-    asio::write(*user_sock, asio::buffer(ss.str()));
-    // prove this is multithreading
+      string response_string;
+      beast::flat_buffer buffer;
+      http::read(stream, buffer, res);
+      stringstream ss;
+      ss << res;
+      asio::write(*user_sock, asio::buffer(ss.str()));
+      // prove this is multithreading
+      // cout << "I AM SLEEPING" << endl;
+      // sleep(1000);
+    }
+
+    catch (...) {
+      cout << "What?" << endl;
+    }
   }
 };
 
